c4f53a0328720dda9d6b88e061eb4d4aba4e868c
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-11 18:26:43 +0800

    feat: change response to csv

--- backend/stock_data/routes/stock.py
+++ backend/stock_data/routes/stock.py
@@ -1,8 +1,14 @@
 from routes.api import api
 from services.stock import get_history
+from flask import  send_file
 
 @api.get('/stock/<ticker>')
 def get_stock_history_route(ticker):
-    stock = get_history(ticker)
+    file_path, filename = get_history(ticker)
 
-    return stock
+    return send_file(
+        file_path,
+        as_attachment=True,
+        download_name=filename,
+        mimetype='text/csv'
+    )
--- backend/stock_data/services/stock.py
+++ backend/stock_data/services/stock.py
@@ -1,8 +1,20 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 from utils import normalize_price
+import csv
+import os
 
-def get_history(ticker: str) -> list[dict]:
+dir_name = 'result'
+
+os.makedirs(dir_name, exist_ok=True)
+
+def get_history(ticker: str) -> tuple[str, str]:
+    filename = f'{ticker.upper()}_history.csv'
+    file_path = os.path.join(dir_name, filename)
+
+    if os.path.exists(file_path):
+        return file_path, filename
+
     yf_ticker = yf.Ticker(f'{ticker.upper()}.JK')
 
     end_date = datetime.now() + timedelta(days=1)
@@ -14,20 +26,25 @@ def get_history(ticker: str) -> list[dict]:
         interval='1d',
         auto_adjust=False,
     )
-    print(df)
 
-    result = []
+    df = df.iloc[::-1]
 
-    for index, row in df.iterrows():
-        result.append({
-            'date': index.strftime('%Y-%m-%d'),
-            'open': normalize_price(row['Open']),
-            'close': normalize_price(row['Close']),
-            'high': normalize_price(row['High']),
-            'low': normalize_price(row['Low']),
-            'volume': int(row['Volume']),
-        })
+    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(
+            file,
+            fieldnames=['date', 'open', 'close', 'high', 'low', 'volume']
+        )
 
-    result.reverse()
+        writer.writeheader()
 
-    return result
+        for index, row in df.iterrows():
+            writer.writerow({
+                'date': index.strftime('%Y-%m-%d'), # type: ignore
+                'open': normalize_price(row['Open']),
+                'close': normalize_price(row['Close']),
+                'high': normalize_price(row['High']),
+                'low': normalize_price(row['Low']),
+                'volume': int(row['Volume']),
+            })
+
+    return file_path, filename
