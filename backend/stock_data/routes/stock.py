ea2de4249d1d44c23f99c98e1626eb1272071d41
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-12 18:35:56 +0800

    feat: create financials, balance sheet and broker summary service

--- backend/stock_data/routes/stock.py
+++ backend/stock_data/routes/stock.py
@@ -1,14 +1,47 @@
 from routes.api import api
-from services.stock import get_history
+from services.stock import get_price_historical, get_financials, get_balance_sheet, get_broker_summary
 from flask import  send_file
 
-@api.get('/stock/<ticker>')
-def get_stock_history_route(ticker):
-    file_path, filename = get_history(ticker)
+@api.get('/stock/<ticker>/price-historical')
+def get_price_historical_route(ticker):
+	file_path, filename = get_price_historical(ticker)
 
-    return send_file(
-        file_path,
-        as_attachment=True,
-        download_name=filename,
-        mimetype='text/csv'
-    )
+	return send_file(
+		file_path,
+		as_attachment=True,
+		download_name=filename,
+		mimetype='text/csv'
+	)
+
+@api.get('/stock/<ticker>/financials')
+def get_financials_route(ticker: str):
+	file_path, filename = get_financials(ticker)
+
+	return send_file(
+		file_path,
+		as_attachment=True,
+		download_name=filename,
+		mimetype='text/csv'
+	)
+
+@api.get('/stock/<ticker>/balance-sheet')
+def get_balance_sheet_route(ticker: str):
+	file_path, filename = get_balance_sheet(ticker)
+
+	return send_file(
+		file_path,
+		as_attachment=True,
+		download_name=filename,
+		mimetype='text/csv'
+	)
+
+@api.get('/stock/<ticker>/broker-summary')
+def get_broker_summary_route(ticker: str):
+	file_path, filename = get_broker_summary(ticker)
+
+	return send_file(
+		file_path,
+		as_attachment=True,
+		download_name=filename,
+		mimetype='text/csv'
+	)
--- backend/stock_data/services/stock.py
+++ backend/stock_data/services/stock.py
@@ -1,50 +1,195 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 from utils import normalize_price
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 import csv
 import os
+import requests
 
 dir_name = 'result'
 
 os.makedirs(dir_name, exist_ok=True)
 
-def get_history(ticker: str) -> tuple[str, str]:
-    filename = f'{ticker.upper()}_history.csv'
-    file_path = os.path.join(dir_name, filename)
+def get_price_historical(ticker: str) -> tuple[str, str]:
+	filename = f'{ticker.upper()}_history.csv'
+	file_path = os.path.join(dir_name, filename)
 
-    if os.path.exists(file_path):
-        return file_path, filename
+	if os.path.exists(file_path):
+		return file_path, filename
 
-    yf_ticker = yf.Ticker(f'{ticker.upper()}.JK')
+	yf_ticker = yf.Ticker(f'{ticker.upper()}.JK')
 
-    end_date = datetime.now() + timedelta(days=1)
-    start_date = end_date - timedelta(days=90)
+	end_date = datetime.now() + timedelta(days=1)
+	start_date = end_date - timedelta(days=90)
 
-    df = yf_ticker.history(
-        start=start_date.strftime('%Y-%m-%d'),
-        end=end_date.strftime('%Y-%m-%d'),
-        interval='1d',
-        auto_adjust=False,
-    )
+	df = yf_ticker.history(
+		start=start_date.strftime('%Y-%m-%d'),
+		end=end_date.strftime('%Y-%m-%d'),
+		interval='1d',
+		auto_adjust=False,
+	)
 
-    df = df.iloc[::-1]
+	df = df.iloc[::-1]
 
-    with open(file_path, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(
-            file,
-            fieldnames=['date', 'open', 'close', 'high', 'low', 'volume']
-        )
+	with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+		writer = csv.DictWriter(
+			file,
+			fieldnames=['date', 'open', 'close', 'high', 'low', 'volume']
+		)
 
-        writer.writeheader()
+		writer.writeheader()
 
-        for index, row in df.iterrows():
-            writer.writerow({
-                'date': index.strftime('%Y-%m-%d'), # type: ignore
-                'open': normalize_price(row['Open']),
-                'close': normalize_price(row['Close']),
-                'high': normalize_price(row['High']),
-                'low': normalize_price(row['Low']),
-                'volume': int(row['Volume']),
-            })
+		for index, row in df.iterrows():
+			writer.writerow({
+				'date': index.strftime('%Y-%m-%d'), # type: ignore
+				'open': normalize_price(row['Open']),
+				'close': normalize_price(row['Close']),
+				'high': normalize_price(row['High']),
+				'low': normalize_price(row['Low']),
+				'volume': int(row['Volume']),
+			})
 
-    return file_path, filename
+	return file_path, filename
+
+
+def get_financials(ticker: str) -> tuple[str, str]:
+	filename = f'{ticker.upper()}_financials.csv'
+	file_path = os.path.join(dir_name, filename)
+
+	if os.path.exists(file_path):
+		return file_path, filename
+	
+	yf_ticker = yf.Ticker(f'{ticker.upper()}.JK')
+
+	df = yf_ticker.get_financials(freq='quarterly')
+	df = df.T
+
+	float_columns = [
+		'BasicEPS',
+		'DilutedEPS',
+	]
+
+	for column in df.columns:
+		df[column] = df[column].fillna(0)
+
+		if column not in float_columns:
+			df[column] = df[column].astype(int)
+		else:
+			df[column] = df[column].astype(float).round(2)
+
+	df.to_csv(file_path, float_format='%.2f', index_label='date')
+
+	return file_path, filename
+
+
+def get_balance_sheet(ticker: str) -> tuple[str, str]:
+	filename = f'{ticker.upper()}_balance_sheet.csv'
+	file_path = os.path.join(dir_name, filename)
+
+	if os.path.exists(file_path):
+		return file_path, filename
+	
+	yf_ticker = yf.Ticker(f'{ticker.upper()}.JK')
+
+	df = yf_ticker.get_balance_sheet(freq='quarterly')
+	df = df.T
+
+	for column in df.columns:
+		df[column] = df[column].fillna(0)
+		df[column] = df[column].astype(int)
+
+	df.to_csv(file_path, float_format='%.2f', index_label='date')
+
+	return file_path, filename
+
+def get_broker_summary(ticker: str) -> tuple[str, str]:
+	filename = f'{ticker.upper()}broker_summary.csv'
+	file_path = os.path.join(dir_name, filename)
+
+	if os.path.exists(file_path):
+		return file_path, filename
+
+	url = os.getenv('NEOBDM_URL')
+	sessionId = os.getenv('NEOBDM_SESSIONID')
+	csrfmiddlewaretoken = os.getenv('NEOBDM_CSRF_TOKEN')
+
+	end_date = datetime.now() + timedelta(days=1)
+	start_date = end_date - timedelta(days=90)
+
+	data = {
+		'tick': ticker,
+		'start_date': start_date.strftime('%d %b %Y'),
+		'end_date': end_date.strftime('%d %b %Y'),
+		'event': 'load',
+		'foreign_only': False,
+		'domestic_only': False,
+		'net': True,
+		'show_broker_inventory': False,
+		'csrfmiddlewaretoken': csrfmiddlewaretoken,
+	}
+
+	headers = {
+		'Referer': url
+	}
+
+	cookies = {
+		'sessionid': sessionId,
+		'csrftoken': csrfmiddlewaretoken,
+	}
+
+	res = requests.post(
+		url,
+		data=data,
+		headers=headers,
+		cookies=cookies
+	)
+
+	broksum_html_str = res.json()['broksum_html']
+
+	soup = BeautifulSoup(broksum_html_str, 'html.parser')
+	rows = soup.select('tbody tr')
+
+	with open(file_path, 'w', newline='', encoding='utf-8') as file:
+		writer = csv.writer(file)
+
+		writer.writerow([
+			'buy_broker',
+			'buy_lot',
+			'buy_value',
+			'buy_avg',
+
+			'sell_broker',
+			'sell_lot',
+			'sell_value',
+			'sell_avg',
+		])
+
+		for row in rows:
+			cols = row.find_all('td')
+
+			if len(cols) < 8:
+				continue
+
+			buy_broker = cols[0].get_text(strip=True)
+			buy_lot = int(cols[1].get_text(strip=True).replace(',', ''))
+			buy_value = cols[2].get_text(strip=True)
+			buy_avg = float(cols[3].get_text(strip=True).replace(',', ''))
+
+			sell_broker = cols[4].get_text(strip=True)
+			sell_lot = int(cols[5].get_text(strip=True).replace(',', ''))
+			sell_value = cols[6].get_text(strip=True)
+			sell_avg = float(cols[7].get_text(strip=True).replace(',', ''))
+
+			writer.writerow([
+				buy_broker,
+				buy_lot,
+				buy_value,
+				buy_avg,
+				sell_broker,
+				sell_lot,
+				sell_value,
+				sell_avg,
+			])
+
+	return file_path, filename
