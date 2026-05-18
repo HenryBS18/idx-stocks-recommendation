cc0063e3b28c8aaf3ca74e0ae27b0119b90f6a62
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-18 18:13:45 +0800

    feat: set log level

--- backend/core/src/main.ts
+++ backend/core/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix('/api')
+  app.useLogger(process.env.NODE_ENV === 'production' ? ['error', 'warn', 'fatal'] : ['verbose', 'debug', 'log', 'warn', 'error', 'fatal'])
   await app.listen(process.env.PORT ?? 4000)
 }
 bootstrap()
