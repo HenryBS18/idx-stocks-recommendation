eae5949ec2229ea6a9a0903fa047f6c2a5dbddac
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-19 22:38:16 +0800

    fix: update production log level

--- backend/core/src/main.ts
+++ backend/core/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix('/api')
-  app.useLogger(process.env.NODE_ENV === 'production' ? ['error', 'warn', 'fatal'] : ['verbose', 'debug', 'log', 'warn', 'error', 'fatal'])
+  app.useLogger(process.env.NODE_ENV === 'production' ? ['error', 'warn', 'fatal', 'log'] : ['verbose', 'debug', 'log', 'warn', 'error', 'fatal'])
   app.useGlobalInterceptors(new LoggingInterceptor())
   await app.listen(process.env.PORT ?? 4000)
 }
