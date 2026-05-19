8feeb820f4e58bf8544d38f59bea591817ea0efc
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-19 22:10:45 +0800

    feat: add redis logger

--- backend/core/src/app.module.ts
+++ backend/core/src/app.module.ts
@@ -1,6 +1,6 @@
 import KeyvRedis from '@keyv/redis'
 import { CacheModule } from '@nestjs/cache-manager'
-import { Module } from '@nestjs/common'
+import { Logger, Module, OnModuleInit } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { CommonModule } from './common/common.module'
 import { StockModule } from './stock/stock.module'
@@ -12,7 +12,7 @@ import { StockModule } from './stock/stock.module'
       isGlobal: true,
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        const redisUrl = configService.getOrThrow<string>('REDIS_URL')
+        const redisUrl = configService.get<string>('REDIS_URL')
         const redis = new KeyvRedis(redisUrl)
 
         return {
@@ -24,4 +24,33 @@ import { StockModule } from './stock/stock.module'
     CommonModule
   ]
 })
-export class AppModule { }
+export class AppModule implements OnModuleInit {
+  constructor(private readonly configService: ConfigService) { }
+
+  async onModuleInit() {
+    const redisUrl = this.configService.get<string>('REDIS_URL')
+    const redis = new KeyvRedis(redisUrl)
+
+    redis.on('connect', () => {
+      Logger.log('Connected', 'Redis')
+    })
+
+    redis.on('reconnecting', () => {
+      Logger.log('Reconnecting', 'Redis')
+    })
+
+    redis.on('disconnect', () => {
+      Logger.log('Disconnected', 'Redis')
+    })
+
+    redis.on('error', (err) => {
+      Logger.error(err, 'Redis')
+    })
+
+    try {
+      await redis.client.connect()
+    } catch (error) {
+      if (error instanceof Error) Logger.error(error.message, 'Redis')
+    }
+  }
+}
