f9481030b6490bd58a6556bdcbc200c195ac443f
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-19 17:16:09 +0800

    feat: create redis config

--- backend/core/src/app.module.ts
+++ backend/core/src/app.module.ts
@@ -1,10 +1,27 @@
+import KeyvRedis from '@keyv/redis'
 import { CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { CommonModule } from './common/common.module'
 import { StockModule } from './stock/stock.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CacheModule.register({ isGlobal: true }), StockModule, CommonModule]
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const redisUrl = configService.getOrThrow<string>('REDIS_URL')
+        const redis = new KeyvRedis(redisUrl)
+
+        return {
+          stores: [redis]
+        }
+      }
+    }),
+    StockModule,
+    CommonModule
+  ]
 })
 export class AppModule { }
