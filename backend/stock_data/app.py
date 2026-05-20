7e248df5b589c2b29f7abf019cc633d01f003c73
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-05-20 19:51:16 +0800

    refactor: seperate app into its own file

---
+++ backend/stock_data/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from dotenv import load_dotenv
+from routes.api import api
+import os
+
+class App:
+	def __init__(self):
+		load_dotenv()
+		
+		self.app = Flask(__name__)
+
+		self.app.register_blueprint(api, url_prefix='/api')
+	
+	def run(self) -> None:
+		host = os.getenv('HOST')
+		port = int(os.getenv('PORT'))
+		debug = os.getenv('ENV') == 'development'
+
+		self.app.run(host=host, port=port, debug=debug)
--- backend/stock_data/main.py
+++ backend/stock_data/main.py
@@ -1,22 +1,4 @@
-from flask import Flask
-from dotenv import load_dotenv
-from routes.api import api
-import os
-
-class App:
-	def __init__(self):
-		load_dotenv()
-		
-		self.app = Flask(__name__)
-
-		self.app.register_blueprint(api, url_prefix='/api')
-
-	def run(self):
-		host = os.getenv('HOST')
-		port = int(os.getenv('PORT'))
-		debug = os.getenv('ENV') == 'development'
-
-		self.app.run(host=host, port=port, debug=debug)
+from app import App
 
 app = App()
 
